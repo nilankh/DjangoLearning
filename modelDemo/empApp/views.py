2ae6ba15621ace90c144b150bc445177184e1d12
nilank nikhil <nilanknikhil@gmail.com>
2022-07-08 12:33:57 +0530

    Learning djngo Use the model in view

--- modelDemo/empApp/views.py
+++ modelDemo/empApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from empApp.models import Employee
 
 # Create your views here.
+def employeedata(request):
+    employees = Employee.objects.all()
+    empDict = {'employees':employees}
+    return render(request, 'empApp/employee.html', empDict)
