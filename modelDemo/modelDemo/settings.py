604ddff63f8fe8cd0e5e98beab23a24cc2b67210
nilank nikhil <nilanknikhil@gmail.com>
2022-07-08 12:00:27 +0530

    learnign django using mysql database

--- modelDemo/modelDemo/settings.py
+++ modelDemo/modelDemo/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'modelDemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'employeedb',
+        'USER':'root',
+        'PASSWORD':'nilank123'
     }
 }
 
