664504dbf714df0c8c3f7d43c8b771b73d242687
nilank nikhil <nilanknikhil@gmail.com>
2022-07-25 11:25:07 +0530

    Learned about csrf token & created the template

--- formsDemo/formsApp/views.py
+++ formsDemo/formsApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+def userRegistrationView(request):
+    form = forms.UserRegistrationFrom
+    return render(request, 'formsDemo/userRegistration.html', {'form': form})
