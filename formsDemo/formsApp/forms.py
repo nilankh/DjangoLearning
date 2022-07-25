ceeca4104aa6534075e8937b45011e6edf3e7bc1
nilank nikhil <nilanknikhil@gmail.com>
2022-07-25 12:11:29 +0530

    Writing custom clean methods & learned apply to validate

--- formsDemo/formsApp/forms.py
+++ formsDemo/formsApp/forms.py
@@ -7,3 +7,16 @@ class UserRegistrationForm(forms.Form):
     email= forms.CharField()
     gender = forms.CharField(widget=forms.Select(choices=GENDER))
     password = forms.CharField(widget=forms.PasswordInput)
+    ssn = forms.IntegerField()
+
+    def clean_firstName(self):
+        inputFirstName = self.cleaned_data['firstName']
+        if len(inputFirstName) > 20:
+            raise forms.ValidationError('The max length of firstName is 20 characters')
+        return inputFirstName
+
+    def clean_email(self):
+        inputEmail = self.cleaned_data['email']
+        if inputEmail.find('@')== -1:
+            raise forms.ValidationError('The email shoudl contain @')
+        return inputEmail
