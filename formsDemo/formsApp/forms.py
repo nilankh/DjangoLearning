68b4be6557e9a52f54a5c07dab68e6aabfc23723
nilank nikhil <nilanknikhil@gmail.com>
2022-07-25 12:16:53 +0530

    single cleaned method

--- formsDemo/formsApp/forms.py
+++ formsDemo/formsApp/forms.py
@@ -9,14 +9,28 @@ class UserRegistrationForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
     ssn = forms.IntegerField()
 
-    def clean_firstName(self):
-        inputFirstName = self.cleaned_data['firstName']
+    # def clean_firstName(self):
+    #     inputFirstName = self.cleaned_data['firstName']
+    #     if len(inputFirstName) > 20:
+    #         raise forms.ValidationError('The max length of firstName is 20 characters')
+    #     return inputFirstName
+
+    # def clean_email(self):
+    #     inputEmail = self.cleaned_data['email']
+    #     if inputEmail.find('@')== -1:
+    #         raise forms.ValidationError('The email shoudl contain @')
+    #     return inputEmail
+
+# Learning about single clean method
+    def clean(self):
+        user_cleaned_data = super().clean()
+        inputFirstName = user_cleaned_data['firstName']
+        inputEmail = user_cleaned_data['email']
         if len(inputFirstName) > 20:
             raise forms.ValidationError('The max length of firstName is 20 characters')
-        return inputFirstName
-
-    def clean_email(self):
-        inputEmail = self.cleaned_data['email']
         if inputEmail.find('@')== -1:
             raise forms.ValidationError('The email shoudl contain @')
-        return inputEmail
+        
+
+
+
