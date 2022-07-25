0db0dda01826ab45149949deb27617bc1d9e9adf
nilank nikhil <nilanknikhil@gmail.com>
2022-07-25 11:53:11 +0530

    Learned about different types of form fields

--- formsDemo/formsApp/forms.py
+++ formsDemo/formsApp/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class UserRegistrationForm(forms.Form):
+    GENDER = [('male','MALE'),('female','FEMALE')]
     firstName= forms.CharField()
     lastName= forms.CharField()
     email= forms.CharField()
+    gender = forms.CharField(widget=forms.Select(choices=GENDER))
+    password = forms.CharField(widget=forms.PasswordInput)
