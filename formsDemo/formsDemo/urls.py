775446a009c33220d64902267255ca0cd3bd530d
nilank nikhil <nilanknikhil@gmail.com>
2022-07-25 11:34:26 +0530

    Configure the CSRF token and URLs

--- formsDemo/formsDemo/urls.py
+++ formsDemo/formsDemo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from formsDemo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('userRegistration/',views.userRegistrationView)
 ]
