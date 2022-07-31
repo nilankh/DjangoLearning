5a6c6b985b162d0b79dde9f7bfda76489babfb87
nilank nikhil <nilanknikhil@gmail.com>
2022-07-31 10:20:18 +0530

    Create the add project view

--- modelFormsDemo/modelForms/views.py
+++ modelFormsDemo/modelForms/views.py
@@ -10,4 +10,13 @@ def listProjects(request):
     projectList = Project.objects.all()
     return render(request, 'modelForms/listProjects.html', {'projects':projectList})
 
-    
+
+def addProject(request):
+    form = ProjectForm()
+    if(form.method == 'POST'):
+        form = MovieForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+        return index(request)
+
+    return render(request, 'modelForms/addProject.html', {'form':form})
