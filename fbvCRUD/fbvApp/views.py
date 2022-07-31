66df8a1cd99e3e9ab61efb5e240180f04f776525
nilank nikhil <nilanknikhil@gmail.com>
2022-07-31 15:44:47 +0530

    Implemented read

--- fbvCRUD/fbvApp/views.py
+++ fbvCRUD/fbvApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from fbvApp.models import Student
 
 # Create your views here.
+def getStudent(request):
+    students = Student.objects.all()
+    return render(request, 'fbvApp/index.html', {'students', students})
