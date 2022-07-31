670737b28bf9799f8e4a25e25e53d7b01c38c904
nilank nikhil <nilanknikhil@gmail.com>
2022-07-31 17:03:09 +0530

    Implemented create

--- fbCrud/fbvApp/views.py
+++ fbCrud/fbvApp/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from fbvApp.models import Student
+from fbvApp.forms import StudentForm
 
 # Create your views here.
 def getStudent(request):
     students = Student.objects.all()
     return render(request, 'fbvApp/index.html', {'students':students})
+
+def createStudent(request):
+    form = StudentForm()
+    if(request.method == 'POST'):
+        form = StudentForm(request.POST)
+        if(form.is_valid):
+            form.save()
+        return redirect('/')
+    return render(request, 'fbvApp/create.html', {'form':form})
