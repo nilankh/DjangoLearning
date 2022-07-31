bd6311743c711cd941ab2cfba97d505d2d124517
nilank nikhil <nilanknikhil@gmail.com>
2022-07-31 10:15:21 +0530

    create the views

--- modelFormsDemo/modelForms/views.py
+++ modelFormsDemo/modelForms/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from modelForms.forms import ProjectForm
+from modelForms.forms import Project
 
 # Create your views here.
+def index(request):
+    return render(request, 'modelForms/index.html')
+
+def listProjects(request):
+    projectList = Project.objects.all()
+    return render(request, 'modelForms/listProjects.html', {'projects':projectList})
+
+    
