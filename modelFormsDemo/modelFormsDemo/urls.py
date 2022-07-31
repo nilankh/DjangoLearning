49eadde18a1d34df2f782bfdeb2d12b3997ac9d9
nilank nikhil <nilanknikhil@gmail.com>
2022-07-31 10:37:07 +0530

    Configure urls

--- modelFormsDemo/modelFormsDemo/urls.py
+++ modelFormsDemo/modelFormsDemo/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from modelForms import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('listProjects/', views.listProjects),
+    path('addProject/', views.addProject)
 ]
