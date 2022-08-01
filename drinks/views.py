72ab85f1713e97c6dddb6fbe6b5b8a6992d0a45b
nilank nikhil <nilanknikhil@gmail.com>
2022-08-01 15:41:40 +0530

    completed

--- drinks/views.py
+++ drinks/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 @api_view(['GET','POST'])
-def drink_list(request):
+def drink_list(request, format=None):
     # Get all drinks
     # serialize them
     #return JSON
@@ -21,7 +21,7 @@ def drink_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def drink_detail(request, id):
+def drink_detail(request, id, format=None):
     try:
         drink = Drink.objects.get(pk=id)
     except Drink.DoesNotExist:
